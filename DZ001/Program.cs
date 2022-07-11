3bda147b22dfedaddacefd1b834d8a7acc9850a9
Inna Osipova <rutheva_inna@mail.ru>
2022-07-11 19:08:10 +0300

    К-во четных числе

---
+++ DZ001/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] RandomArray ()
+{
+    int[] arrayRand = new int [20];
+    for (int i = 0; i < 20; i++)
+    {
+        arrayRand [i] = new Random().Next(100, 1000);
+    }
+    return arrayRand;
+
+}
+
+int Сoucounting (int[] arr)
+{
+   int cou = 0;
+   for (int i = 0; i < 20; i++)
+   {
+     Console.Write (arr[i]+", ");
+     if ((arr[i] % 2) == 0)  cou++;
+   }
+   Console.WriteLine();
+   return cou;
+}
+
+int[] array = RandomArray();
+int count = Сoucounting(array);
+
+Console.WriteLine($"Количество четных чисел массива = {count}");
