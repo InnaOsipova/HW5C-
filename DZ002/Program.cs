ac54898647b36a8742b1c8ac727b1ee813f357db
Inna Osipova <rutheva_inna@mail.ru>
2022-07-11 19:23:21 +0300

    Сумма нечетных э-тов

---
+++ DZ002/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] RandomArray ()
+{
+    int[] arrayRand = new int [20];
+    for (int i = 0; i < 20; i++)
+    {
+        arrayRand [i] = new Random().Next(100, 1000);
+        Console.Write (arrayRand[i]+", ");
+    }
+    Console.WriteLine();
+    return arrayRand;
+
+}
+
+int Сoucounting (int[] arr)
+{
+   int sum = 0;
+   for (int i = 1; i < 20; i = i+2)
+   {
+      sum = sum + arr[i];
+   }
+   
+   return sum;
+}
+
+int[] array = RandomArray();
+int summa = Сoucounting(array);
+
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях = {summa}");
